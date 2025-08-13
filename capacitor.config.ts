127a2c6462dc95d7cf099c8dea326965a5f94f71
اسلام جمال السيد احمد <ijahmed@moj.gov.sa>
2025-08-13 09:53:06 +0300

    build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,19 @@
-{
-  "appId": "com.prayer.reminder",
-  "appName": "Prayer Reminder",
-  "webDir": "dist",
-  "server": {
-    "androidScheme": "https"
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.prayer.reminder',
+  appName: 'Prayer Reminder',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
   },
-  "plugins": {
-    "LocalNotifications": {
-      "smallIcon": "ic_stat_icon_config_sample",
-      "iconColor": "#28A745",
-      "sound": "beep.wav"
+  plugins: {
+    LocalNotifications: {
+      smallIcon: 'ic_stat_icon_config_sample',
+      iconColor: '#28A745',
+      sound: 'beep.wav'
     }
   }
-}
+};
+
+export default config;
